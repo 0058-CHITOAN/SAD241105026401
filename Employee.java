27e44742b6e2b585e0b0613738333c9184127745
0058-CHITOAN <162245498+0058-CHITOAN@users.noreply.github.com>
2024-11-27 20:40:09 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String employeeID;
+
+    public Employee(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+}
